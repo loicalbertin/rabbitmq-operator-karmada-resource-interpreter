59b29ba4fedaaa25aa42ce37c1adcd50084e256c
Albertin Loic <loic.albertin@atos.net>
2022-10-13 15:13:26 +0200

    Add retain on image
    Signed-off-by: Albertin Loic <loic.albertin@atos.net>

--- app/workloadwebhook.go
+++ app/workloadwebhook.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 
 	rabbitv1beta1 "github.com/rabbitmq/cluster-operator/api/v1beta1"
@@ -39,10 +40,10 @@ func (e *rabbitClusterInterpreter) Handle(ctx context.Context, req interpreter.R
 		return e.responseWithExploreReplica(rabbitCluster)
 	case configv1alpha1.InterpreterOperationReviseReplica:
 		return e.responseWithExploreReviseReplica(rabbitCluster, req)
-	// case configv1alpha1.InterpreterOperationRetain:
-	// 	return e.responseWithExploreRetaining(rabbitCluster, req)
-	// case configv1alpha1.InterpreterOperationAggregateStatus:
-	// 	return e.responseWithExploreAggregateStatus(rabbitCluster, req)
+	case configv1alpha1.InterpreterOperationRetain:
+		return e.responseWithExploreRetaining(rabbitCluster, req)
+	case configv1alpha1.InterpreterOperationAggregateStatus:
+		return e.responseWithExploreAggregateStatus(rabbitCluster, req)
 	// case configv1alpha1.InterpreterOperationInterpretHealth:
 	// 	return e.responseWithExploreInterpretHealth(rabbitCluster)
 	// case configv1alpha1.InterpreterOperationInterpretStatus:
@@ -73,48 +74,59 @@ func (e *rabbitClusterInterpreter) responseWithExploreReviseReplica(rabbit *rabb
 	return interpreter.PatchResponseFromRaw(req.Object.Raw, marshaledBytes)
 }
 
-// func (e *rabbitClusterInterpreter) responseWithExploreRetaining(desiredRabbit *rabbitv1beta1.RabbitmqCluster, req interpreter.Request) interpreter.Response {
-// 	if req.ObservedObject == nil {
-// 		err := fmt.Errorf("nil observedObject in exploreReview with operation type: %s", req.Operation)
-// 		return interpreter.Errored(http.StatusBadRequest, err)
-// 	}
-// 	observerRabbitCluster := &rabbitv1beta1.RabbitmqCluster{}
-// 	err := e.decoder.DecodeRaw(*req.ObservedObject, observerRabbitCluster)
-// 	if err != nil {
-// 		return interpreter.Errored(http.StatusBadRequest, err)
-// 	}
+func (e *rabbitClusterInterpreter) responseWithExploreRetaining(desiredRabbit *rabbitv1beta1.RabbitmqCluster, req interpreter.Request) interpreter.Response {
+	if req.ObservedObject == nil {
+		err := fmt.Errorf("nil observedObject in exploreReview with operation type: %s", req.Operation)
+		return interpreter.Errored(http.StatusBadRequest, err)
+	}
+	observerRabbitCluster := &rabbitv1beta1.RabbitmqCluster{}
+	err := e.decoder.DecodeRaw(*req.ObservedObject, observerRabbitCluster)
+	if err != nil {
+		return interpreter.Errored(http.StatusBadRequest, err)
+	}
 
-// 	// Suppose we want to retain the `.spec.paused` field of the actual observed rabbitCluster object in member cluster,
-// 	// and prevent from being overwritten by karmada controller-plane.
-// 	wantedRabbitCluster := desiredRabbit.DeepCopy()
-// 	wantedRabbitCluster.Spec.Paused = observerRabbitCluster.Spec.Paused
-// 	marshaledBytes, err := json.Marshal(wantedRabbitCluster)
-// 	if err != nil {
-// 		return interpreter.Errored(http.StatusInternalServerError, err)
-// 	}
-// 	return interpreter.PatchResponseFromRaw(req.Object.Raw, marshaledBytes)
-// }
+	// We need to retain the `.spec.image` field of the actual observed rabbitCluster object in member cluster,
+	// and prevent from being overwritten by karmada controller-plane. Otherwise we run into an infinite loop of both
+	// Karmada & RabbitMQ operator updating this field.
+	wantedRabbitCluster := desiredRabbit.DeepCopy()
+	wantedRabbitCluster.Spec.Image = observerRabbitCluster.Spec.Image
 
-// func (e *rabbitClusterInterpreter) responseWithExploreAggregateStatus(rabbitCluster *rabbitv1beta1.RabbitmqCluster, req interpreter.Request) interpreter.Response {
-// 	wantedRabbitCluster := rabbitCluster.DeepCopy()
-// 	var readyReplicas int32
-// 	for _, item := range req.AggregatedStatus {
-// 		if item.Status == nil {
-// 			continue
-// 		}
-// 		status := &rabbitv1beta1.RabbitmqClusterStatus{}
-// 		if err := json.Unmarshal(item.Status.Raw, status); err != nil {
-// 			return interpreter.Errored(http.StatusInternalServerError, err)
-// 		}
-// 		readyReplicas += status.ReadyReplicas
-// 	}
-// 	wantedRabbitCluster.Status.ReadyReplicas = readyReplicas
-// 	marshaledBytes, err := json.Marshal(wantedRabbitCluster)
-// 	if err != nil {
-// 		return interpreter.Errored(http.StatusInternalServerError, err)
-// 	}
-// 	return interpreter.PatchResponseFromRaw(req.Object.Raw, marshaledBytes)
-// }
+	marshaledBytes, err := json.Marshal(wantedRabbitCluster)
+	if err != nil {
+		return interpreter.Errored(http.StatusInternalServerError, err)
+	}
+	return interpreter.PatchResponseFromRaw(req.Object.Raw, marshaledBytes)
+}
+
+func (e *rabbitClusterInterpreter) responseWithExploreAggregateStatus(rabbitCluster *rabbitv1beta1.RabbitmqCluster, req interpreter.Request) interpreter.Response {
+	wantedRabbitCluster := rabbitCluster.DeepCopy()
+	var readyReplicas int32
+	for _, item := range req.AggregatedStatus {
+		if item.Status == nil {
+			continue
+		}
+		status := &rabbitv1beta1.RabbitmqClusterStatus{}
+		if err := json.Unmarshal(item.Status.Raw, status); err != nil {
+			return interpreter.Errored(http.StatusInternalServerError, err)
+		}
+		for _, rmqCondition := range status.Conditions {
+			if rmqCondition.Type == "AllReplicasReady" && rmqCondition.Status == corev1.ConditionTrue {
+				readyReplicas += 1
+			}
+		}
+
+	}
+	if readyReplicas == *req.DesiredReplicas {
+		wantedRabbitCluster.Status.SetCondition("AllReplicasReady", corev1.ConditionTrue, "aggregated status", "True")
+	} else {
+		wantedRabbitCluster.Status.SetCondition("AllReplicasReady", corev1.ConditionFalse, "aggregated status", "False")
+	}
+	marshaledBytes, err := json.Marshal(wantedRabbitCluster)
+	if err != nil {
+		return interpreter.Errored(http.StatusInternalServerError, err)
+	}
+	return interpreter.PatchResponseFromRaw(req.Object.Raw, marshaledBytes)
+}
 
 // func (e *rabbitClusterInterpreter) responseWithExploreInterpretHealth(rabbitCluster *rabbitv1beta1.RabbitmqCluster) interpreter.Response {
 // 	healthy := pointer.Bool(false)
